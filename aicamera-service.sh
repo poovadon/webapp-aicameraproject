83254e111fbae8278ae5564fba4f331ab26720e5
poovadon <134037280+poovadon@users.noreply.github.com>
2025-05-24 21:24:33 +0700

    Update aicamera-service.sh

--- aicamera-service.sh
+++ aicamera-service.sh
@@ -1,1 +1,8 @@
-aicamera-service.sh
+#!/bin/bash
+cd ../../../..
+# เปลี่ยน directory ไปยัง project directory
+cd path/webapp-aicameraproject/django/cameraproject/
+# เปิด virtual environment
+source venv/bin/activate
+# รัน Django server บน 0.0.0.0:8000 แบบ background
+python3 manage.py  runserver 0.0.0.0:8000 &
